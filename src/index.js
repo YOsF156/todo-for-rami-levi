aac7a90dbc146765c0c4e93e26e886c41bc6c0ec
YOsF156 <yosistar@gmail.com>
2023-06-27 01:53:54 +0300

    fabicon

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
+
 import './index.css';
 import App from './App';
 
+// ...
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   // <React.StrictMode>
-  <App />
-  //</React.StrictMode>
+  <>
+    <FontAwesomeIcon icon={faShoppingCart} style={{ display: 'none' }} />
+    <App />
+  </>
+  // {/* </React.StrictMode>, */}
 );
-
-
